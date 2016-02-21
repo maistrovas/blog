a03f9cb838ceb2008e109962bb4e877953ae0633
Maistrovas <maistrovas@gmail.com>
2016-02-21 23:08:51 +0200

    Admin panel changing

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -1,5 +1,12 @@
+
 from django.contrib import admin
 from .models import Post
+from django.utils import timezone 
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'published_date')
+    list_filter = ['published_date']
+    search_fields = ['title']
+admin.site.register(Post, PostsAdmin)
