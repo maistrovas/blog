56c8a77510a44e9d81fa9a0fc5c3c4522682fff3
Maistrovas <maistrovas@gmail.com>
2016-03-04 19:30:07 +0200

    Adding comment option

--- my_blog/models.py
+++ my_blog/models.py
@@ -23,6 +23,10 @@ class Post(models.Model):
     def unpublish(self):
         self.published_date = None
         self.save()
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+    
     def __str__(self):
     	return self.title
 
@@ -38,6 +42,8 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment=True
         self.save()
+
+    
 
     def __str__(self):
         return self.text
--- my_blog/views.py
+++ my_blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Post
+from .models import Post, Comment
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 # Create your views here.
 
 
@@ -71,6 +71,31 @@ def post_unpublish(request, pk):
 	return redirect('my_blog.views.post_detail', pk=pk)
 
 
+def add_comment_to_post(request,pk):
+	post = get_object_or_404(Post,pk=pk)
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect('my_blog.views.post_detail', pk=post.pk)
+	else:
+		form = CommentForm()
+	return render(request, 'my_blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('my_blog.views.post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('my_blog.views.post_detail', pk=post_pk)
 
 
 
